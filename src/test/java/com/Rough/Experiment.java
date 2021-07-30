58a5d5d3d7a36bc72add34747ef8ea78fb2f817f
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-07-30 17:04:43 +0600

    Update Experiment.java

--- src/test/java/com/Rough/Experiment.java
+++ src/test/java/com/Rough/Experiment.java
@@ -5,6 +5,8 @@ public class Experiment {
     public static void main(String[] args) {
 
         System.out.println(System.getProperty("user.dir"));
+        
+         System.out.println(System.getProperty("user.dir2"));
     }
 
 
